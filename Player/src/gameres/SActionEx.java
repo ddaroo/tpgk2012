6f55e4b5d12bc0c46f5ba6f224bf3d6bd7ce1ab9
DAroo <ddaroo@gmail.com>
2012-06-17 20:02:01 +0200

    Kodowanie znaków, poprawionych kilka warunków granicznych

--- Player/src/gameres/SActionEx.java
+++ Player/src/gameres/SActionEx.java
@@ -1,6 +1,9 @@
 package gameres;
 
 import java.io.*;
+import java.util.logging.Level;
+
+import main.SApp;
 
 /**
  * Wymienia literki gracza
@@ -16,11 +19,18 @@ public class SActionEx extends SAction {
 	
 	@Override
 	public void writeData(DataOutputStream dout) throws IOException {
+		String logmsg = "Akcja - Ex: ";
 		dout.writeByte(ActionId.EX_LETTERS.ordinal());
 		dout.writeByte(mletters.length);
 		for(char let : mletters) {
-			dout.writeByte(let);
+			byte output = SCharacterCoder.processOutput(let);
+			dout.writeByte(output);
+			
+			logmsg = logmsg
+					.concat(Character.toString(let))
+					.concat(" ");
 		}
+		SApp.mlog.log(Level.INFO, logmsg);
 	}
 
 	private char[] mletters;
